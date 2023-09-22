8d73c23466ee624dc73f244c495fa601ac8efe6d
fedeWax <cha_lino_23@hotmail.com>
2023-09-21 21:07:03 -0700

    mejoras menu

--- src/app/components/sider/index.tsx
+++ src/app/components/sider/index.tsx
@@ -1,27 +1,30 @@
 import { useState } from 'react'
 import { Layout, Menu } from "antd"
+import { HomeOutlined, PoweroffOutlined } from "@ant-design/icons"
 import { auth } from "@src/app/firebase";
 
 const Sider = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <Layout.Sider collapsible collapsed={collapsed}>
+    <Layout.Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={setCollapsed}
+    >
       <Menu
         theme="dark"
         mode="inline"
         items={[
           {
-            key: '1',
-            label: 'nav 1',
-          },
-          {
-            key: '2',
-            label: 'nav 2',
+            key: 'inicio',
+            label: 'Inicio',
+            icon: <HomeOutlined />
           },
           {
             key: '3',
             label: 'Cerrar sesión',
+            icon: <PoweroffOutlined />,
             onClick: () => auth.signOut()
           },
         ]}
--- src/app/hooks/useIsSmallScreen.tsx
+++ src/app/hooks/useIsSmallScreen.tsx
@@ -7,7 +7,7 @@ const useIsSmallScreen = () => {
   const { xs, sm, md, lg, xl, xxl } = useBreakpoint();
 
   const isSmallScreen = useMemo(() => {
-    return xs || sm && (!md && !lg && !xl && !xxl);
+    return (xs || sm) && (!md && !lg && !xl && !xxl);
   }, [xs, sm, md, lg, xl, xxl]);
 
   return isSmallScreen;
