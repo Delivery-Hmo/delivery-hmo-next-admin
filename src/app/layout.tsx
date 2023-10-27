7d34c3a69463b78eee18724553e9b5b21423d9ec
fedeWax <cha_lino_23@hotmail.com>
2023-10-26 19:59:20 -0700

    fix rutas privadaas con y sin sesion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,37 @@
 'use client';
 import './globals.css';
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { App, ConfigProvider, Layout } from "antd";
 import AuthProvider from "@src/context/auth";
 import ErrorBoundary from "@src/components/errorBoundary";
 import Sider from "@src/components/sider";
 import Breadcrumb from "@src/components/breadcrumb";
+import { usePathname, useRouter } from "next/navigation";
+import FullLoader from "@src/components/fullLoader";
+import { User } from "firebase/auth";
 
 export default function RootLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (loading) return;
+
+    if (user && pathname === "/") {
+      router.push('/inicio');
+    }
+
+    if (!user) {
+      router.push('/');
+    }
+  }, [pathname, user, loading]);
+
   return (
     <html lang="en">
       <body>
@@ -23,20 +43,27 @@ export default function RootLayout({
           }}
         >
           <App>
-            <AuthProvider>
-              <Layout style={{ height: "100vh" }}>
-                <Sider />
-                <ErrorBoundary>
-                  <Layout.Content style={{ margin: 20 }}>
-                    <Breadcrumb />
-                    {children}
-                  </Layout.Content>
-                </ErrorBoundary>
-              </Layout>
-            </AuthProvider>
+            {
+              <AuthProvider
+                onLoadUser={_user => {
+                  setUser(_user);
+                  setLoading(false);
+                }}
+              >
+                <Layout style={{ height: "100vh" }}>
+                  <Sider />
+                  <ErrorBoundary>
+                    <Layout.Content style={{ margin: 20 }}>
+                      <Breadcrumb />
+                      {children}
+                    </Layout.Content>
+                  </ErrorBoundary>
+                </Layout>
+              </AuthProvider>
+            }
           </App>
         </ConfigProvider >
       </body>
-    </html>
+    </html >
   );
 }
--- src/context/auth/index.tsx
+++ src/context/auth/index.tsx
@@ -1,36 +1,27 @@
 import { useEffect, useState, useContext, createContext, FC, ReactNode } from "react";
 import { User, onIdTokenChanged } from "firebase/auth";
-import { usePathname, useRouter } from "next/navigation";
 import { auth } from "@src/firebase";
 import FullLoader from "@src/components/fullLoader";
 
 interface Props {
   children: ReactNode;
+  onLoadUser: (user: User | null) => void;
 }
 
 const AuthContext = createContext<{ user: User | null, loading: boolean; }>({
   user: null,
-  loading: true
+  loading: true,
 });
 
-const AuthProvider: FC<Props> = ({ children }) => {
+const AuthProvider: FC<Props> = ({ children, onLoadUser }) => {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState<Boolean>(true);
-  const router = useRouter();
-  const pathname = usePathname();
 
   useEffect(() => {
     const uns = onIdTokenChanged(auth, async (user: User | null) => {
-      if (user && pathname === "/") {
-        router.push('/inicio');
-      }
-
-      if (!user) {
-        router.push('/');
-      }
-
       setUser(user);
       setLoading(false);
+      onLoadUser(user);
     });
 
     return () => {
